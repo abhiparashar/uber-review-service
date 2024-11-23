2a12d46124b26fec2d8975aa8d42fbc1051870fa
Abhishek Parashar <aparashar103@gmail.com>
2024-11-23 15:25:14 +0530

    basic jpa setup done

---
+++ src/test/java/uberReviewService/uber/review/service/UberReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uberReviewService.uber.review.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
